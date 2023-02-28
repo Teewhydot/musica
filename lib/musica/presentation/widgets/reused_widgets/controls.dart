c661ec9cad6a533cb7647a0f7b8195311aaab748
teewhydot <tchipsical@gmail.com>
2023-02-28 23:19:33 +0100

    Fix: Ui bug on collection_details_page.dart. add: New custom files for ui.

---
+++ lib/musica/presentation/widgets/reused_widgets/controls.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+
+class Controls extends StatelessWidget {
+  final AudioPlayer audioPlayer;
+  const Controls({super.key, required this.audioPlayer});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+        stream: audioPlayer.playerStateStream,
+        builder: (context, snapshot) {
+          final playerState = snapshot.data;
+          final processingState = playerState?.processingState;
+          final playing = playerState?.playing;
+          if (processingState == ProcessingState.loading ||
+              processingState == ProcessingState.buffering) {
+            return Container(
+              margin: const EdgeInsets.all(8.0),
+              width: 32.0,
+              height: 32.0,
+              child: const CircularProgressIndicator(),
+            );
+          } else {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.skip_previous),
+                  iconSize: 40.0,
+                  onPressed: audioPlayer.hasPrevious
+                      ? () => audioPlayer.seekToPrevious()
+                      : null,
+                ),
+                IconButton(
+                  icon: Icon(playing! ? Icons.pause : Icons.play_arrow),
+                  iconSize: 40.0,
+                  onPressed: () {
+                    playing ? audioPlayer.pause() : audioPlayer.play();
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.skip_next),
+                  iconSize: 40.0,
+                  onPressed: audioPlayer.hasNext
+                      ? () => audioPlayer.seekToNext()
+                      : null,
+                ),
+              ],
+            );
+          }
+        });
+  }
+}
